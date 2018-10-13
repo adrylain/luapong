3bfacaec4bbed1d7ac15050159993ddce4fa03ec
adrylain <32313532+adrylain@users.noreply.github.com>
2018-10-12 22:00:29 -0400

    Fedora installer.

---
+++ Installers/FedoraInstaller.sh
@@ -0,0 +1,9 @@
+sudo dnf install SDL2
+sudo dnf install SDL2_tff-devel
+sudo dnf install SDL2_image-devel
+sudo dnf install SDL2_mixer-devel
+sudo dnf install lua
+sudo dnf install lua-devel
+sudo dnf install luarocks
+
+sudo luarocks install lua-sdl2
